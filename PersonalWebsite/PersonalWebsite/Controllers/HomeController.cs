74c4fc9d02ef96670d8272b9d52e769eda45daba
sgollen <38255138+sgollen@users.noreply.github.com>
2019-02-11 00:49:50 -0500

    Updated Personal Website

---
+++ PersonalWebsite/PersonalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonalWebsite.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web.Routing;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+        public class NoDirectAccessAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                if (filterContext.HttpContext.Request.UrlReferrer == null ||
+                            filterContext.HttpContext.Request.Url.Host != filterContext.HttpContext.Request.UrlReferrer.Host)
+                {
+                    filterContext.Result = new RedirectToRouteResult(new
+                              RouteValueDictionary(new { controller = "Home", action = "Sent", area = "" }));
+                }
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(EmailFormModel model)
+        {
+            ViewBag.Message = "There is Some ERROR";
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("receiver@outlook.com"));
+                message.From = new MailAddress("sender@outlook.com");
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "workingemail@outlook.com",
+                        Password = "yourpassword"
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+
+            }
+            return View(model);
+        }
+        [NoDirectAccess]
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ PersonalWebsite/PersonalWebsite/Models/EmailFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebsite.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required, Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
